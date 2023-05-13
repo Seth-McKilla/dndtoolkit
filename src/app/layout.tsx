8acecbdbc9d6ab8f37561a78c6a0f368b564bdfc
Seth McCullough <seth@endevrs.dev>
2023-05-13 18:03:45 -0400

    Add navbar to overall layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google"
 import Link from "next/link"
+import { WrenchScrewdriverIcon } from "@heroicons/react/24/solid"
 
 import "./globals.css"
 
@@ -11,18 +12,19 @@ export const metadata = {
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  // const isDev = process.env.NODE_ENV === "development"; // Uncomment to seed the db
-  // if (isDev) (await import("@/lib/seed")).seed(); // Uncomment to seed the db
+  // if (process.env.NODE_ENV === "development") (await import("@/lib/seed")).seed() // Uncomment to seed the db
 
   return (
     <html lang="en">
       <body className={`overflow-x-hidden ${inter.className}`}>
         <nav className="flex items-center justify-center p-4">
-          <Link href="/" className="text-2xl text-center">
-            D&D
+          <Link href="/" className="flex items-center justify-center text-2xl">
+            <div className="w-12 h-12 rounded-full border-2 border-white flex items-center justify-center text-sm">
+              D <WrenchScrewdriverIcon className="h-4 w-4" /> D
+            </div>
           </Link>
         </nav>
-        <main className="flex min-h-screen flex-col items-center justify-between p-4 pt-12 md:p-24 -mt-16">
+        <main className="flex min-h-screen flex-col items-center justify-between p-4 pt-16 md:p-24 -mt-20">
           <div className="flex flex-col items-center justify-center space-y-4 md:space-y-8">
             {children}
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Card from "@/components/Card"
+import LinkCard from "@/components/LinkCard"
 
 export default async function HomePage() {
   return (
     <>
       <p className="text-4xl md:text-6xl font-bold text-center mt-2 md:mt-0">D&D Toolkit</p>
-      <Card title="Druid Bestiary" href="/druid-bestiary" />
+      <LinkCard title="Druid Bestiary" href="/druid-bestiary" />
     </>
   )
 }
